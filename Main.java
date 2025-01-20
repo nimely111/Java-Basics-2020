9a9784d390e4126c82af9457dcd5405ae649f2ce
Nimely111 <samuelehnimely@gmail.com>
2025-01-20 22:32:05 +0000

    removed unnecessary comments and re adjust the code

--- Main.java
+++ Main.java
@@ -1,18 +1,12 @@
-
-/* forward slash star star forward*/
-//
-
 public class Main {
 
-    public static void main(String[] args) { // Arithematic-Operators
-                                             // + - * / % ++ --
-                                             // Datatype variableName = value;
+    public static void main(String[] args) { 
+        // Arithematic-Operators
+        // + - * / % ++ --
+        // Datatype variableName = value;
 
         int leftOperand = 4; // this is an expression e.g = leftOperand
         int rightOperand = 3;
-
-        // double samProgram = 8.1;
-        // String marthline = "How are you? ";
 
         int additionResult = leftOperand + rightOperand;
 
