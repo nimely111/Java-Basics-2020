cfb17fc0b254f454b51bd73b68a4f30591123e90
Nimely111 <samuelehnimely@gmail.com>
2025-01-20 16:38:47 +0000

    updated

--- Main.java
+++ Main.java
@@ -1,37 +1,28 @@
 
 /* forward slash star star forward*/
 //
-package arithematic_operators;
-/*
 
-*/
+public class Main {
 
-public class Main
-{
+    public static void main(String[] args) { // Arithematic-Operators
+                                             // + - * / % ++ --
+                                             // Datatype variableName = value;
 
-    
-    public static void main(String[] args) 
-{    // Arithematic-Operators 
-    // + - * / % ++ -- 
-    // Datatype variableName = value;
-    
-    int leftOperand = 4; // this is an expression e.g = leftOperand
-    int rightOperand = 3;
-    
-    //double samProgram = 8.1;
-    //String marthline = "How are you? ";
-    
-    int additionResult = leftOperand + rightOperand;
-    
-    
-    System.out.println(leftOperand + rightOperand);
-    System.out.println(leftOperand + " + " + rightOperand + " = " + additionResult);
-    
-   
-   System.out.println(additionResult++);
-    
-    int ModuloResult = leftOperand % rightOperand;
-    System.out.println(ModuloResult);
-  
-}    
+        int leftOperand = 4; // this is an expression e.g = leftOperand
+        int rightOperand = 3;
+
+        // double samProgram = 8.1;
+        // String marthline = "How are you? ";
+
+        int additionResult = leftOperand + rightOperand;
+
+        System.out.println(leftOperand + rightOperand);
+        System.out.println(leftOperand + " + " + rightOperand + " = " + additionResult);
+
+        System.out.println(additionResult++);
+
+        int ModuloResult = leftOperand % rightOperand;
+        System.out.println(ModuloResult);
+
+    }
 }
