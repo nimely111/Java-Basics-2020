b4c2ab9ca88cf13a70fee9b45168999ab33b1255
Nimely111 <samuelehnimely@gmail.com>
2025-01-20 16:34:13 +0000

    using arithmatic operations in java

---
+++ Arithematic_Operators/src/arithematic_operators/Main.java
@@ -0,0 +1,37 @@
+
+/* forward slash star star forward*/
+//
+package arithematic_operators;
+/*
+
+*/
+
+public class Main
+{
+
+    
+    public static void main(String[] args) 
+{    // Arithematic-Operators 
+    // + - * / % ++ -- 
+    // Datatype variableName = value;
+    
+    int leftOperand = 4; // this is an expression e.g = leftOperand
+    int rightOperand = 3;
+    
+    //double samProgram = 8.1;
+    //String marthline = "How are you? ";
+    
+    int additionResult = leftOperand + rightOperand;
+    
+    
+    System.out.println(leftOperand + rightOperand);
+    System.out.println(leftOperand + " + " + rightOperand + " = " + additionResult);
+    
+   
+   System.out.println(additionResult++);
+    
+    int ModuloResult = leftOperand % rightOperand;
+    System.out.println(ModuloResult);
+  
+}    
+}
