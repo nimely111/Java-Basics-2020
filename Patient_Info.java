f33ee5470172cdbdbdbb661db4029620d148a6c2
Nimely111 <samuelehnimely@gmail.com>
2025-01-21 23:05:08 +0000

    add a practice project to this repo

---
+++ Patient_Info.java
@@ -0,0 +1,20 @@
+
+package patient_info;
+
+public class Patient_Info {
+
+    public static void main(String[] args) {
+        // now let us declare our variable
+        // in this program we are going to create a patient data_base.
+
+        String patientName = " John Brown";
+        int patientAge = 21;
+        String patientAddress = " Sinkor";
+        int patientDateOfBirth = 2000;
+
+        System.out.println(
+                "The patient name is, " + patientName + " He is " + patientAge + patientAddress + patientDateOfBirth);
+
+    }/* the patientname is +patientName , He is + patientAge */
+
+}
