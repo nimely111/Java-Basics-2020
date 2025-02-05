4c56a40839efb05dd27657391ed0998656005743
Nimely111 <samuelehnimely@gmail.com>
2025-02-05 20:29:16 +0000

    updated

--- While_Loop1.java
+++ While_Loop1.java
@@ -2,25 +2,25 @@
 // a line of code as long as a given condition is true
 // it also serves as a boolean expression True / False
 
-package while_loop1;
+
+
 import java.util.Scanner;
+
 public class While_Loop1 {
 
     public static void main(String[] args) {
         // this is what prompt the user to enter something
-       Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter your number! ");
         int value = scanner.nextInt();
-        
-        if(value > 0)
-        {
+
+        if (value > 0) {
             System.out.println("You have enter a positive number! ");
         }
-        if(value < 0)
-        {
+        if (value < 0) {
             System.out.println("You have enter a negative number! ");
         }
-       
+
     }
-    
+
 }
