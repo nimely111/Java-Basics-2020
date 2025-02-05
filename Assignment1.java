912d98157808c2c3052b0d6b5efd4176188dfb3a
Nimely111 <samuelehnimely@gmail.com>
2025-02-05 20:27:07 +0000

    removed the package

--- Assignment1.java
+++ Assignment1.java
@@ -1,30 +1,27 @@
-// this is question number one
-package assignment1;
 
+// this is question number one
 import java.util.Scanner;
 
 public class Assignment1 {
 
-    public static void main(String[] args) {
-     Scanner scan = new Scanner (System.in);
-     System.out.println("Please enter an integer");
-        
-int occurences = scan.nextInt();
+   public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      System.out.println("Please enter an integer");
 
-int[][] myArray ={{321},{456}, {456}};
-      
-   int i;// declaring of variable
-   
-   // innitialization termination increament
-   for( i = 0; i <= myArray.length; i++)
-{
-      
-   if(i != myArray.length)
-{
-      System.out.println("The most occuring digit is 3. ");  
-}
-}
-          
-}
-    
+      int occurences = scan.nextInt();
+
+      int[][] myArray = { { 321 }, { 456 }, { 456 } };
+
+      int i;// declaring of variable
+
+      // innitialization termination increament
+      for (i = 0; i <= myArray.length; i++) {
+
+         if (i != myArray.length) {
+            System.out.println("The most occuring digit is 3. ");
+         }
+      }
+
+   }
+
 }
