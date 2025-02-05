f9b8a444a965f17aa24140ea40067ada48084a48
Nimely111 <samuelehnimely@gmail.com>
2025-02-05 20:45:10 +0000

    updated

---
+++ Sam.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+public class Sam {
+
+        // @param args the command line arguments
+
+        public static void main(String[] args) {
+                /*
+                 * System.out.println(2 + 3 + "Test " );
+                 * System.out.println("Test " + 2 + 3);
+                 * System.out.println("Test " + 2 * 3);
+                 */
+
+                int x;
+                x = -1;
+                if (x < 0)
+                        x = 1;
+                else
+                        x = 2;
+                System.out.println(x);
+        }
+
+}
--- Weight_Conversion.java
+++ Weight_Conversion.java
@@ -1,25 +1,19 @@
 
-package weight_conversion;
-
-
 import java.util.Scanner;
-
 
 public class Weight_Conversion {
 
-   
     public static void main(String[] args) {
-       // we going to as our user for their weight in pounds
-       // and convert it to kilogram
+        // we going to as our user for their weight in pounds
+        // and convert it to kilogram
         Scanner scanner = new Scanner(System.in);
         System.out.println("Weight in (Lbs): ");
         int weight_in_Lbs = scanner.nextInt();
-        
-       int var =  weight_in_Lbs;
-        double weight_in_kg = weight_in_Lbs * 0.45;
-        System.out.println("weight_in_kg is: "+ weight_in_kg);
 
-      
+        int var = weight_in_Lbs;
+        double weight_in_kg = weight_in_Lbs * 0.45;
+        System.out.println("weight_in_kg is: " + weight_in_kg);
+
     }
-    
+
 }
